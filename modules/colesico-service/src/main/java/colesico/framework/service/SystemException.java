f7967ba45621ba3ecadb8572d5c795f97d2d4a2e
Unknown <vladechko@gmail.com>
2019-03-17 17:23:31 +0300

    SystemException aded

---
+++ modules/colesico-service/src/main/java/colesico/framework/service/SystemException.java
@@ -0,0 +1,22 @@
+package colesico.framework.service;
+
+public class SystemException extends RuntimeException {
+    public SystemException() {
+    }
+
+    public SystemException(String message) {
+        super(message);
+    }
+
+    public SystemException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SystemException(Throwable cause) {
+        super(cause);
+    }
+
+    public SystemException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
