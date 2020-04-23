550e4b372bec85bbfd1a4d4e96a256c2552b7255
colesico <colesico@colesico.net>
2020-04-23 17:33:02 +0300

    javadoc plugin version fix

--- modules/colesico-transaction/src/main/java/colesico/framework/transaction/Transactional.java
+++ modules/colesico-transaction/src/main/java/colesico/framework/transaction/Transactional.java
@@ -28,15 +28,11 @@ public @interface Transactional {
 
     /**
      * Transaction propagation type
-     *
-     * @return
      */
     TransactionPropagation propagation() default TransactionPropagation.REQUIRED;
 
     /**
      * Named qualifier value of TransactionalShell field
-     *
-     * @return
      * @see Named
      */
     String shell() default "";
