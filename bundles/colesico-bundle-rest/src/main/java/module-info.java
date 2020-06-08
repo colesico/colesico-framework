48a7bfffd49d7255ac89e2a27181da4439863b88
colesico <vladechko@gmail.com>
2020-06-08 10:50:16 +0300

    v3.4.0

---
+++ bundles/colesico-bundle-rest/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module colesico.framework.bundle.rest {
+
+    requires transitive colesico.framework.bundle.base;
+
+    requires transitive colesico.framework.http;
+    requires transitive colesico.framework.router;
+    requires transitive colesico.framework.restlet;
+}
