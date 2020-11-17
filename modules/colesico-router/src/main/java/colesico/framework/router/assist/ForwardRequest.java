bf03955572f2b505d320cd2dd22750d03a6163cc
colesico <vladechko@gmail.com>
2020-11-17 11:38:52 +0300

    up

--- modules/colesico-router/src/main/java/colesico/framework/router/assist/ForwardRequest.java
+++ modules/colesico-router/src/main/java/colesico/framework/router/assist/ForwardRequest.java
@@ -51,17 +51,6 @@ public class ForwardRequest implements HttpRequest {
         }
     }
 
-    public static Map<String, String> splitQuery(URL url) throws UnsupportedEncodingException {
-        Map<String, String> query_pairs = new LinkedHashMap<String, String>();
-        String query = url.getQuery();
-        String[] pairs = query.split("&");
-        for (String pair : pairs) {
-            int idx = pair.indexOf("=");
-            query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
-        }
-        return query_pairs;
-    }
-
     @Override
     public HttpMethod getRequestMethod() {
         return parentRequest.getRequestMethod();
