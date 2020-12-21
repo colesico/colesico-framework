76899295e2edf35fd323bb60b5752fdeb935d959
colesico <vladechko@gmail.com>
2020-12-21 10:42:41 +0300

    rpc profile reader up

--- modules/colesico-rpc/src/main/java/colesico/framework/rpc/teleapi/reader/RpcProfileReader.java
+++ modules/colesico-rpc/src/main/java/colesico/framework/rpc/teleapi/reader/RpcProfileReader.java
@@ -1,20 +1,24 @@
 package colesico.framework.rpc.teleapi.reader;
 
 import colesico.framework.profile.Profile;
+import colesico.framework.profile.teleapi.CommonProfileCreator;
 import colesico.framework.profile.teleapi.ProfileSerializer;
 import colesico.framework.rpc.teleapi.BasicEnvelope;
 import colesico.framework.rpc.teleapi.RpcTRContext;
 import colesico.framework.rpc.teleapi.RpcTeleReader;
 
 import javax.inject.Singleton;
+import java.util.Locale;
 
 @Singleton
 public class RpcProfileReader implements RpcTeleReader<Profile> {
 
     protected final ProfileSerializer profileSerializer;
+    protected final CommonProfileCreator commonProfileCreator;
 
-    public RpcProfileReader(ProfileSerializer profileSerializer) {
+    public RpcProfileReader(ProfileSerializer profileSerializer, CommonProfileCreator commonProfileCreator) {
         this.profileSerializer = profileSerializer;
+        this.commonProfileCreator = commonProfileCreator;
     }
 
     @Override
@@ -25,6 +29,9 @@ public class RpcProfileReader implements RpcTeleReader<Profile> {
         } else {
             BasicEnvelope env = (BasicEnvelope) context.getRequest();
             profile = profileSerializer.deserialize(env.getProfile());
+        }
+        if (profile == null) {
+            profile = commonProfileCreator.createCommonProfile(Locale.getDefault());
         }
         return profile;
     }
