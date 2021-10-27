4b7ea8c2325c515c6eba7a70168a5ccebdfcd225
colesico <colesico@gmail.com>
2021-10-27 17:07:37 +0300

    pom ver. up

--- modules/colesico-pebble/src/main/java/colesico/framework/pebble/PebbleTemplater.java
+++ modules/colesico-pebble/src/main/java/colesico/framework/pebble/PebbleTemplater.java
@@ -18,6 +18,7 @@ package colesico.framework.pebble;
 
 import colesico.framework.http.HttpContext;
 import colesico.framework.http.HttpResponse;
+import colesico.framework.http.assist.HttpUtils;
 import colesico.framework.ioc.production.Polysupplier;
 import colesico.framework.pebble.internal.FrameworkExtension;
 import colesico.framework.pebble.internal.PebbleTemplateLoader;
@@ -29,6 +30,7 @@ import colesico.framework.weblet.teleapi.writer.ViewWriter;
 import com.mitchellbosecke.pebble.PebbleEngine;
 import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Provider;
 import javax.inject.Singleton;
@@ -83,7 +85,21 @@ public class PebbleTemplater extends ViewWriter implements HtmlRenderer {
     public void write(ViewResponse viewResponse, WebletTWContext context) {
         Writer writer = evaluate(viewResponse.getViewName(), viewResponse.getModel());
         HttpResponse httpResponse = httpContextProv.get().getResponse();
-        httpResponse.sendText(writer.toString(), HtmlResponse.DEFAULT_CONTENT_TYPE, 200);
+
+        HttpUtils.setHeaders(httpResponse, viewResponse.getHeaders());
+        HttpUtils.setCookies(httpResponse, viewResponse.getCookies());
+
+        String contentType = viewResponse.getContentType();
+        if (StringUtils.isNoneEmpty(contentType)) {
+            contentType = HtmlResponse.DEFAULT_CONTENT_TYPE;
+        }
+
+        int statusCode = viewResponse.getStatusCode();
+        if (statusCode == 0) {
+            statusCode = 200;
+        }
+
+        httpResponse.sendText(writer.toString(), contentType, statusCode);
     }
 
     /**
