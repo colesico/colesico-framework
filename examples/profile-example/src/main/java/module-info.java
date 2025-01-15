d5675765b8f0d55aa02015e7c023096279e5db49
colesico <colesico@gmail.com>
2025-01-15 10:04:22 +0300

    copyright header years up

---
+++ examples/profile-example/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module colesico.framework.example.profile {
+
+    requires transitive colesico.framework.service;
+    requires transitive colesico.framework.profile;
+
+    exports colesico.framework.example.profile;
+
+}
---
+++ examples/profile-example/src/test/java/module-info.java
@@ -0,0 +1,10 @@
+module colesico.framework.test.example.profile {
+
+    requires colesico.framework.example.profile;
+
+    requires org.slf4j;
+    requires org.testng;
+
+    exports colesico.framework.test.example.profile;
+
+}
