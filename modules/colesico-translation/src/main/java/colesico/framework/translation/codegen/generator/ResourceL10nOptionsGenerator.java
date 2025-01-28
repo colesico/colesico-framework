84534d9708c90214b21a83facfd597b73d10730b
colesico <colesico@gmail.com>
2025-01-28 17:17:17 +0300

    up

---
+++ modules/colesico-translation/src/main/java/colesico/framework/translation/codegen/generator/ResourceL10nOptionsGenerator.java
@@ -0,0 +1,11 @@
+package colesico.framework.translation.codegen.generator;
+
+import com.palantir.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+
+public class ResourceL10nOptionsGenerator {
+   // TypeSpec.Builder options = TypeSpec.classBuilder();
+    //    producerBuilder.addModifiers(Modifier.PUBLIC);
+
+}
