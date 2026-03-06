6e6906dc72af26856f0383f41da7514b1e5fc7d9
vvl <vvl@vvl.net>
2026-03-06 16:10:03 +0300

    up

--- modules/colesico-profile/src/main/java/colesico/framework/profile/DefaultProfileManager.java
+++ modules/colesico-profile/src/main/java/colesico/framework/profile/DefaultProfileManager.java
@@ -26,9 +26,17 @@ public class DefaultProfileManager implements ProfileManager<DefaultProfile> {
     @Override
     public DefaultProfile readProfile(DataPort dataPort) {
         var profile = ProfileManager.super.readProfile(dataPort);
+
+        if (profile == null){
+            profile = newInstance();
+            profile.setLocale(Locale.getDefault());
+            return profile;
+        }
+
         if (profile.getLocale() == null) {
             profile.setLocale(Locale.getDefault());
         }
+
         return profile;
     }
 }
