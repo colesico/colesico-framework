29bbfc101ca332a9ccd8f21caaed6bf25d191b72
Ларионов Владлен Викторович <vvlarionov@hse.ru>
2026-04-29 10:21:45 +0300

    fix hello world pom

--- examples/helloworld-example/src/main/java/module-info.java
+++ examples/helloworld-example/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module colesico.framework.example.helloworld {
 
     requires transitive colesico.framework.weblet;
     requires transitive colesico.framework.undertow;
-    requires transitive java.net.http;
     requires org.slf4j;
 
     exports colesico.framework.example.helloworld;
--- examples/helloworld-example/src/test/java/module-info.java
+++ examples/helloworld-example/src/test/java/module-info.java
@@ -1,6 +1,7 @@
 module colesico.framework.test.example.helloworld {
 
     requires colesico.framework.example.helloworld;
+    requires java.net.http;
 
     requires org.slf4j;
     requires org.testng;
