6c2dcad3bce9edd4d047dac5e4561bbfc77771a4
Ларионов Владлен Викторович <vvlarionov@hse.ru>
2026-06-30 12:33:15 +0300

    up

--- modules/colesico-tele-http/src/main/java/colesico/framework/telehttp/writer/ObjectWriter.java
+++ modules/colesico-tele-http/src/main/java/colesico/framework/telehttp/writer/ObjectWriter.java
@@ -2,6 +2,7 @@ package colesico.framework.telehttp.writer;
 
 import colesico.framework.config.Config;
 import colesico.framework.config.DefaultMessage;
+import colesico.framework.ioc.production.Classed;
 import colesico.framework.telehttp.MediaType;
 import colesico.framework.telehttp.TeleHttpWriteOptions;
 import colesico.framework.telehttp.TeleHttpWriter;
@@ -18,7 +19,7 @@ public class ObjectWriter implements TeleHttpWriter<Object, TeleHttpWriteOptions
 
     protected final ValueResponseWriter<ValueResponse<Object>, TeleHttpWriteOptions> writer;
 
-    public ObjectWriter(ValueResponseWriter writer) {
+    public ObjectWriter(@Classed(ValueResponseWriter.Config.class) ValueResponseWriter writer) {
         this.writer = writer;
     }
 
