a11240d7c88151170b24de5e87e7be1d89140b25
vvl <vvl@vvl.net>
2026-09-09 13:34:06 +0300

    fix transitive

--- modules/colesico-hikaricp/src/main/java/module-info.java
+++ modules/colesico-hikaricp/src/main/java/module-info.java
@@ -2,9 +2,9 @@ module colesico.framework.hikaricp {
 
     requires transitive colesico.framework.config;
     requires transitive java.sql;
+    requires transitive com.zaxxer.hikari;
 
     requires org.slf4j;
-    requires com.zaxxer.hikari;
 
     // API
     exports colesico.framework.hikaricp;
